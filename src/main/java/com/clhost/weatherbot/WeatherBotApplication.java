8d9427493ac8385d41a54369c20cdc615876033c
clhost <lchostinc@gmail.com>
2018-07-12 12:17:36 +0300

    initial

---
+++ src/main/java/com/clhost/weatherbot/WeatherBotApplication.java
@@ -0,0 +1,13 @@
+package com.clhost.weatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class WeatherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBotApplication.class, args);
+	}
+}
