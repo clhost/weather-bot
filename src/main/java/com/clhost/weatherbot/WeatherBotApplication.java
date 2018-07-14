d275f5568d5011cb6a3b27a7d639e193bc8e9105
clhost <lchostinc@gmail.com>
2018-07-14 15:32:29 +0300

    добавил инициализацию контекста бота

--- src/main/java/com/clhost/weatherbot/WeatherBotApplication.java
+++ src/main/java/com/clhost/weatherbot/WeatherBotApplication.java
@@ -2,12 +2,12 @@ package com.clhost.weatherbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
-
 public class WeatherBotApplication {
-
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(WeatherBotApplication.class, args);
 	}
 }
