b925db3bdfee952be6ee67f5e41d341c78418bba
clhost <lchostinc@gmail.com>
2018-07-15 22:28:46 +0300

    скрипты для тестов

---
+++ src/test/resources/interstate_insert.sql
@@ -0,0 +1,6 @@
+insert into inter_state(id, subscription_id, w_type) values (1, 1, 1);
+insert into inter_state(id, subscription_id, w_type) values (2, 2, 2);
+insert into inter_state(id, subscription_id, w_type) values (3, 3, 3);
+insert into inter_state(id, subscription_id, w_type) values (4, 4, 4);
+insert into inter_state(id, subscription_id, w_type) values (5, 5, 5);
+insert into inter_state(id, subscription_id, w_type) values (6, 6, 6);
