c59e3f6da265e8c553e7095b68573b78d21aac51
clhost <lchostinc@gmail.com>
2018-07-16 17:47:08 +0300

    удалил out

--- out/test/resources/subscription_insert.sql
+++
@@ -1,6 +0,0 @@
-insert into subscription(id, user_id, hours, city) values (1, 200001, 1, 'Москва');
-insert into subscription(id, user_id, hours, city) values (2, 200002, 2, 'New-York');
-insert into subscription(id, user_id, hours, city) values (3, 200003, 3, 'London');
-insert into subscription(id, user_id, hours, city) values (4, 200004, 4, 'Санкт-Петербург');
-insert into subscription(id, user_id, hours, city) values (5, 200005, 5, 'Иркутск');
-insert into subscription(id, user_id, hours, city) values (6, 200001, 3, 'Иркутск');
